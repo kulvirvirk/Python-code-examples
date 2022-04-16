76b23b65528a6bd89efab5333f84e816db109b94
kulvirvirk <kvirkdev@gmail.com>
2022-04-16 09:37:42 -0700

    Added if-else example
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04152022 Python Variables/variables_test.py
+++ 04152022 Python Variables/variables_test.py
@@ -1,6 +1,15 @@
 # declare variable and use is later in fuction
-
+'''
 age = 22
 name = "John Doe"
 
 print("User name is: " + name + " the age is: " + str(age))
+'''
+userName = input("Hello, what is your name? ")
+print("Nice to meet you " + userName + "!")
+
+user_response = input("Do you like learning python?(Yes/No) ")
+if user_response == "Yes" or user_response == "yes":
+    print("Awesome! keep going.")
+else: 
+    print("O no, that makes me sad :(")
