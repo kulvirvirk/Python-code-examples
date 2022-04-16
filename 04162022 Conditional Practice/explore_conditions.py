1ebbf345def0526d5a03de5c0bc19ba9b0d98930
kulvirvirk <kvirkdev@gmail.com>
2022-04-16 12:42:30 -0700

    initial commit
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

---
+++ 04162022 Conditional Practice/explore_conditions.py
@@ -0,0 +1,24 @@
+''' 
+COMPARISON OPERATORS
+==                   # equal values
+!=                   # not equal
+>                    # left operand is greater than right operand
+<                    # left operand is less than right operand
+>=                   # left operand is greater than or equal to right operand
+<=                   # left operand is less than or equal to right operand
+
+LOGICAL OPERATORS
+1 < 2 and 4 > 1 # True
+1 > 3 or 4 > 1  # True
+1 is not 4      # True
+not True        # False
+1 not in [2,3,4]# True
+
+if <condition that evaluates to boolean>:
+  # perform action1
+elif <condition that evaluates to boolean>:
+  # perform action2
+else:
+  # perform action3
+
+'''
