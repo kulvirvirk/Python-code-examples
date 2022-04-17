065de9996c200160cbb55cd365730c83c76a47bf
kulvirvirk <kvirkdev@gmail.com>
2022-04-17 16:42:17 -0700

    added random message code
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04172022 Booleans/random_fortune_cookie.py
+++ 04172022 Booleans/random_fortune_cookie.py
@@ -2,5 +2,16 @@
 
 import random
 
-random_number = random.randint(1,10)
-print(f"You will have a good day. \nYour lucky number is:{random_number}")
+random_lucky_number = random.randint(1,10)
+random_fortune_msg = random.randint(1,3)
+
+
+print(f"Have a good day. \nYour lucky number is:{random_lucky_number}")
+
+if random_fortune_msg == 1:
+    print("You'll win lotto today!")
+if random_fortune_msg == 2:
+    print("You'll get married this year!")
+if random_fortune_msg == 3:
+    print("You'll get a promotion this year!")
+
