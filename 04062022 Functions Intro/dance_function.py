9b9e970ba0a917a9c29a38ca723e7e5f89bf4a4e
kulvirvirk <kvirkdev@gmail.com>
2022-04-16 19:59:04 -0700

    initial commit to dance_function
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

---
+++ 04062022 Functions Intro/dance_function.py
@@ -0,0 +1,12 @@
+# write a function to do 'shimmy' dance move. 
+
+
+def shimmy_move():
+    print("~~~The Shimmy~~~")
+    print("Take a step to right and stomp!")
+    print("Take a step to left and stomp!")
+    print("Shake shake shake!!!")
+
+
+shimmy_move()
+shimmy_move()
