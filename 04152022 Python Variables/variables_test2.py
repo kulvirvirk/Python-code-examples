9ef423bedf753188db545ac582a901265d38965a
kulvirvirk <kvirkdev@gmail.com>
2022-04-17 14:29:23 -0700

    add example of regular print statement.
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04152022 Python Variables/variables_test2.py
+++ 04152022 Python Variables/variables_test2.py
@@ -4,4 +4,11 @@ day = 21
 month = "October"
 tempratue = -4
 
+
+
+#print using f string
 print(f"Today is {day} of {month}, and the temprature outside is {tempratue}.")
+
+#print using regular print statement.
+# Note: you have to covert the Int data for month and temprature to string first. 
+print("Today is " + str(day) + " of " + month + ", and the temprature outside is " + str(tempratue) + ".")
