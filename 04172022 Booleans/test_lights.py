9eec7aa223833444c3299bbe189490d1be96a281
kulvirvirk <kvirkdev@gmail.com>
2022-04-17 14:47:02 -0700

    initial commit
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

---
+++ 04172022 Booleans/test_lights.py
@@ -0,0 +1,11 @@
+# use boolean to set the status of light
+
+from traceback import print_tb
+
+
+is_light_on = True
+
+if is_light_on == True:
+    print("Light is ON!")
+else:
+    print("Light is OFF!")
