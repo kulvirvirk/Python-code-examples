3d5b261cda8098f17bdaa72f21f9380513eb7fb6
kulvirvirk <kvirkdev@gmail.com>
2022-04-17 16:36:21 -0700

    initial commit for random_fortune cookie
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

---
+++ 04172022 Booleans/random_fortune_cookie.py
@@ -0,0 +1,6 @@
+# use a random number generator to provide the random number to user
+
+import random
+
+random_number = random.randint(1,10)
+print(f"You will have a good day. \nYour lucky number is:{random_number}")
