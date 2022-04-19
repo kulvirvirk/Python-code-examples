12f45076b8e52495986dccf3e01cd4f51ab9d518
kulvirvirk <kvirkdev@gmail.com>
2022-04-19 06:56:29 -0700

    for loop example added for movies list
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04192022 Loop Examples/for_loop.py
+++ 04192022 Loop Examples/for_loop.py
@@ -1,9 +1,13 @@
 
 # create a list of fruits and print each item using for loop.
 fruits = ['apple', 'banana', 'mango']
-for i in fruits:
-    print(i)
+for fruit in fruits:
+    print(fruit)
 
+# from list of movies, print each item using for loop.
+my_fav_movies = ['Forrest Gump', 'Rang de Basanti', 'Jumanji', 'Ironman', 'End Game']
+for movie in my_fav_movies:
+    print(movie)
 
 # create a list of numbers using range() function and print each item using for loop.
 # note: range funciton give u a list of numbers based on 'start' and 'stop' values
@@ -15,3 +19,4 @@ for number in range(10):
 # create a list of numbers using range() function and print 'Hello' using for loop.
 for number in range(5):    
     print('Hello')
+
