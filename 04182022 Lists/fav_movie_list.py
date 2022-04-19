47cbf4eb785ae8c011edfc1a6bc170ccd03077f5
Kulvir Virk <kulvir.s.virk@kp.org>
2022-04-18 21:39:14 -0700

    added  append() and insert() example
    Signed-off-by: Kulvir Virk <kulvir.s.virk@kp.org>

--- 04182022 Lists/fav_movie_list.py
+++ 04182022 Lists/fav_movie_list.py
@@ -10,3 +10,10 @@ print(my_fav_movies[1])
 # find the length of the list - use len() method
 print(len(my_fav_movies))
 
+# add a new item to list 
+my_fav_movies.append('1984')
+print(my_fav_movies)
+
+# insert a new item to list 
+my_fav_movies.insert(2, 'PK')
+print(my_fav_movies)
