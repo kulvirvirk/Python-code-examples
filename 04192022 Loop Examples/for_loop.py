b367f49cb810438c41e878ebc4c988da649eeea6
kulvirvirk <kvirkdev@gmail.com>
2022-04-19 06:48:07 -0700

    added example of range()
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04192022 Loop Examples/for_loop.py
+++ 04192022 Loop Examples/for_loop.py
@@ -1,6 +1,12 @@
 
-
 # create a list of fruits and print each item using for loop.
 fruits = ['apple', 'banana', 'mango']
 for i in fruits:
     print(i)
+
+
+# create a list of numbers using range() function and print each item using for loop.
+# note: range funciton give u a list of numbers based on 'start' and 'stop' values
+# 
+for number in range(10):    
+    print(number)
