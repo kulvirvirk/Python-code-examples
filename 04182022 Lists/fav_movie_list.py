2cb5d7f6481c97525cfc9f50d7359304dfe46e3a
Kulvir Virk <kulvir.s.virk@kp.org>
2022-04-18 21:33:58 -0700

    added len() method example
    Signed-off-by: Kulvir Virk <kulvir.s.virk@kp.org>

--- 04182022 Lists/fav_movie_list.py
+++ 04182022 Lists/fav_movie_list.py
@@ -1,6 +1,12 @@
 # list and print your favorite movies list
+# print the second item on the list
+# 
 
-my_fav_movies = ['Forrest Gump', 'Rang de Basanti', 'Jumanji']
+my_fav_movies = ['Forrest Gump', 'Rang de Basanti', 'Jumanji', 'Ironman', 'End Game']
 
 print(my_fav_movies)
 print(my_fav_movies[1])
+
+# find the length of the list - use len() method
+print(len(my_fav_movies))
+
