2b0bc781ac8b3eb5be8e0e16e2ab55e179d56292
kulvirvirk <kvirkdev@gmail.com>
2022-04-20 06:48:12 -0700

    Formatted code for readability
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04192022 Loop Examples/for_loop.py
+++ 04192022 Loop Examples/for_loop.py
@@ -1,22 +1,28 @@
-
+print('--------------------------------')
 # create a list of fruits and print each item using for loop.
 fruits = ['apple', 'banana', 'mango']
 for fruit in fruits:
     print(fruit)
+print('--------------------------------')
 
 # from list of movies, print each item using for loop.
 my_fav_movies = ['Forrest Gump', 'Rang de Basanti', 'Jumanji', 'Ironman', 'End Game']
 for movie in my_fav_movies:
     print(movie)
+print('--------------------------------')
+
 
 # create a list of numbers using range() function and print each item using for loop.
 # note: range funciton give u a list of numbers based on 'start' and 'stop' values
 # 
 for number in range(10):    
     print(number)
+print('--------------------------------')
+
 
 
 # create a list of numbers using range() function and print 'Hello' using for loop.
 for number in range(5):    
     print('Hello')
+print('--------------------------------')
 
