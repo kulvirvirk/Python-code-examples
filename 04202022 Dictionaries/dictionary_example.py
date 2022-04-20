b67fd2a17e7889a253d8296b91786074152f68de
kulvirvirk <kvirkdev@gmail.com>
2022-04-20 06:59:00 -0700

    initial commit
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

---
+++ 04202022 Dictionaries/dictionary_example.py
@@ -0,0 +1,6 @@
+
+# create a dictionary called Students, add student name as key and their GPA as a value
+
+students = {"Nick": 4, "Tom": 3.2, "James":3.3, "Ram":4.0}
+#print dictionary
+print(students)
