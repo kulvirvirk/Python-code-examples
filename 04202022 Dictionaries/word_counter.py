16900e86ea7c7ad9bd692f67bd855908ef65f471
kulvirvirk <kvirkdev@gmail.com>
2022-04-20 21:46:27 -0700

    initial commit
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

---
+++ 04202022 Dictionaries/word_counter.py
@@ -0,0 +1,10 @@
+# from a give text count how many times each word appears. 
+
+text = '''et qui proident voluptate excepteur enim Lorem ea id ipsum veniam
+ culpa eu anim amet cupidatat ad ad culpa voluptate dolore esse et culpa pariatur
+  consequat proident irure id voluptate officia nulla in aliqua est nisi 
+  consectetur non aliqua duis nostrud Lorem et aliquip laborum qui do eu 
+  commodo in aute qui nostrud elit sint aliqua quis eu in dolor magna aliqua
+   consequat esse elit aute esse eu consequat consectetur enim deserunt esse 
+   occaecat do ad est sit nostrud et ex est labore enim ad consequat 
+adipisicing laboris est ex voluptate sunt velit enim aliqua ut minim officia ut ut'''
