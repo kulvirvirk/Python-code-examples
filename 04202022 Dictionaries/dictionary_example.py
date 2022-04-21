ebbc611230f1612885b3f2efd730b7d8cb3af346
kulvirvirk <kvirkdev@gmail.com>
2022-04-20 21:20:57 -0700

    initial commit
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04202022 Dictionaries/dictionary_example.py
+++ 04202022 Dictionaries/dictionary_example.py
@@ -7,14 +7,18 @@ from turtle import setundobuffer
 students = {"Nick": 4, "Tom": 3.2, "James":3.3, "Ram":4.0}
 #print dictionary
 print(students)
+print('--------------------------------')
 
 #access specific student's GPA by using the key
 print(students["Nick"])
+print('--------------------------------')
 
 #add new item to the dictionary
 students["Bob"] = 2.9
 print(students)
+print('--------------------------------')
 
 #delete an item from the dictionary
 del(students['Tom'])
 print(students)
+print('--------------------------------')
