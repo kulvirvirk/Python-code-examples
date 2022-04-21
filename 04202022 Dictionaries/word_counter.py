ea098885c7ea0df3f831ab05ef2fd4b9dc6a119b
kulvirvirk <kvirkdev@gmail.com>
2022-04-20 22:03:14 -0700

    added dictionary example
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04202022 Dictionaries/word_counter.py
+++ 04202022 Dictionaries/word_counter.py
@@ -8,3 +8,17 @@ text = '''et qui proident voluptate excepteur enim Lorem ea id ipsum veniam
    consequat esse elit aute esse eu consequat consectetur enim deserunt esse 
    occaecat do ad est sit nostrud et ex est labore enim ad consequat 
 adipisicing laboris est ex voluptate sunt velit enim aliqua ut minim officia ut ut'''
+
+#convert the text into a list by using split()
+text_to_list = text.split()
+
+# use dictionary to hold the words as key and count as value. 
+
+word_count = {}
+for word in text_to_list:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+print(word_count)
+print('--------------------------------')
