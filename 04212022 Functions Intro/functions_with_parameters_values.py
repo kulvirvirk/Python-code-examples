e61cfc4d7bfea03bc2d6cb0d1f16415f33c79de7
Kulvir Virk <kulvir.s.virk@kp.org>
2022-04-21 22:17:43 -0700

    add example of function with two parameters
    Signed-off-by: Kulvir Virk <kulvir.s.virk@kp.org>

--- 04212022 Functions Intro/functions_with_parameters_values.py
+++ 04212022 Functions Intro/functions_with_parameters_values.py
@@ -3,11 +3,18 @@
 
 def say_hello(name):
     print(f"Hello {name}!")
-
-
 say_hello("Nick")
+print('--------------------------------')
 
 # use for loop to say hello 5 times and pass the parameter "John"
 for number in range(5):
     say_hello("John")
+print('--------------------------------')
 
+# create add_nums function and pass two numbers as arguments. 
+def add_nums (num1, num2): 
+    print(f"Sum of {num1} and {num2} = {num1 + num2}")
+
+add_nums(5, 2)
+
+print('--------------------------------')
