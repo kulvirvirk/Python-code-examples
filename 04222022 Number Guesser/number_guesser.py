c91e20d4cdfae2265cb0678933d661135c709830
kulvirvirk <kvirkdev@gmail.com>
2022-04-23 08:19:56 -0700

    created a random number generator
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04222022 Number Guesser/number_guesser.py
+++ 04222022 Number Guesser/number_guesser.py
@@ -1,2 +1,6 @@
-# create a program that guesses the number for the user 
-# each loop it checks with user if the need to go higher or lower
+# create a program that lets the user guess the random number picked by the code. 
+# each loop it lets user try the number and tell them to go higher or lower.
+import random
+random_number = random.randint(1, 100)
+
+print(random_number)
