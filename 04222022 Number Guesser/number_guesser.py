c0feb4427178e4b50136e5d60d951f0f8c5037cc
kulvirvirk <kvirkdev@gmail.com>
2022-04-23 08:25:10 -0700

    added the code to capture user's name
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04222022 Number Guesser/number_guesser.py
+++ 04222022 Number Guesser/number_guesser.py
@@ -1,6 +1,9 @@
 # create a program that lets the user guess the random number picked by the code. 
 # each loop it lets user try the number and tell them to go higher or lower.
+
+
 import random
 random_number = random.randint(1, 100)
 
-print(random_number)
+user_name = input("Enter you name to begin the :")
+print(f"Hello {user_name}, let's play... \n\n`````````Random Number Guessing Game`````````\n")
