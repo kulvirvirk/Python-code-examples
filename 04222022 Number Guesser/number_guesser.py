2c0f5386ebf24a9eeea314ea2f12ec759d90c745
kulvirvirk <kvirkdev@gmail.com>
2022-04-23 08:31:21 -0700

    created a fuction to create a random number
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04222022 Number Guesser/number_guesser.py
+++ 04222022 Number Guesser/number_guesser.py
@@ -1,9 +1,13 @@
 # create a program that lets the user guess the random number picked by the code. 
 # each loop it lets user try the number and tell them to go higher or lower.
 
-
+# fuction to generate the random number
 import random
-random_number = random.randint(1, 100)
+def generate_random_num():
+    return random.randint(1, 100)
 
 user_name = input("Enter you name to begin the :")
 print(f"Hello {user_name}, let's play... \n\n`````````Random Number Guessing Game`````````\n")
+
+    
+random_number = generate_random_num()
