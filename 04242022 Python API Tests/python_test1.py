4c077c92e8e01d77d343d02d47edef16653a414a
kulvirvirk <kvirkdev@gmail.com>
2022-04-25 21:56:33 -0700

    parse only items from the json
    Signed-off-by: kulvirvirk <kvirkdev@gmail.com>

--- 04242022 Python API Tests/python_test1.py
+++ 04242022 Python API Tests/python_test1.py
@@ -1,5 +1,12 @@
 import requests
 import json
 
-r = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
-print(r)
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+print(response)     # print the response from the endpoint 
+
+# response.json() returns a JSON object of the result.
+# (if the result was written in JSON format, if not it raises an error). 
+# Python requests are generally used to fetch the content from a particular resource URI.
+
+print(response.json()['items']) 
+
